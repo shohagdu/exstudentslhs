d3512144451cd5f723fb0201a588f70069840cd2
omarshohagdu <omarfaruk@du.ac.bd>
2022-02-09 22:53:25 +0600

    some issue fixed csrf token cache issue kernal update

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <!-- CSRF Token -->
-    <meta name="_token" content="{!! csrf_token() !!}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
